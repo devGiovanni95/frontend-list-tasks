8bcf5610339c913294a3af535b72233ca8518aaa
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-09-07 23:20:56 -0300

    Modified task item for add new buttom

--- src/components/taskItem/index.tsx
+++ src/components/taskItem/index.tsx
@@ -1,16 +1,29 @@
-import { Box, Flex, Text } from "@chakra-ui/react"
+import { Box, Button, Flex, Text } from "@chakra-ui/react"
 import { Task } from "../../interfaces/task"
+import { useUpdateTask } from "../../hooks/mutations/mutationTask"
 
 interface TaskItemProps {
   task: Task
 }
 
 export default function TaskItem({ task }: TaskItemProps) {
+    
+    const { mutate, isLoading } = useUpdateTask()
+
+    const handleUpdateTask = (task: Task) => {
+      mutate({
+        title: task.title,
+        completed: !task.completed
+      })
+    }
+    
   return (
     <Box as="li" bg="gray.800" p={3} borderRadius="md" my={2}>
       <Flex align="center" justify="space-between">
         <Text>{task.title}</Text>
-        <Text>{task.completed ? "Realizada" : "Pendente"}</Text>
+        <Button colorScheme={task.completed ? 'green' : 'red'}
+          onClick={() => { handleUpdateTask(task) }}
+          isLoading={isLoading}>{task.completed ? "Realizada" : "Pendente"}</Button>
       </Flex>
     </Box>
   )
