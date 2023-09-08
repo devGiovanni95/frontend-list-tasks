8a3e7819be63d2b6a9fdfc322e7d62cac18e6735
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-09-07 23:07:46 -0300

    Created new component and refacted homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,23 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Heading } from "@chakra-ui/react";
 import Header from "../../components/header";
 import {useTasks} from "../../hooks/queries/useTasks";
+import TaskForm from "../../components/tasksForm";
 
 export default function Home() {
 
   const {data : tasks } = useTasks()
   
+  
     return (
       <>
         <Header/>
-        <Container>
-          <h1>Home</h1>
-
+          <Container>
+            <Heading mt={5}>
+              Lista de tasks
+            </Heading>
+          <hr />
+            <TaskForm />
+          <hr />
           <ul>
             {tasks?.map((task) => ( // percorrendo o array de tasks e renderizando cada uma delas
               <li key={task.id}>{task.title}</li> // definindo a key como o id da task
