6cee7c151907cbcf7d091c190f761f4a920d1c1a
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-09-07 21:21:42 -0300

    Mapping tasks in homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,23 @@
 import { Container } from "@chakra-ui/react";
 import Header from "../../components/header";
+import useTasks from "../../hooks/useTasks";
 
 export default function Home() {
+
+  const { tasks } = useTasks()
+  
     return (
       <>
         <Header/>
         <Container>
           <h1>Home</h1>
+
+          <ul>
+            {tasks.map((task) => ( // percorrendo o array de tasks e renderizando cada uma delas
+              <li key={task.id}>{task.title}</li> // definindo a key como o id da task
+            ))}
+          </ul>
+
         </Container>
       </>
     )
